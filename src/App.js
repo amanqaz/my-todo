d476abe6b17f2e5a3c4030adb211b3d4da19e5e5
aman <amanisyoyo@gmail.com>
2025-10-16 22:35:11 +0530

    basic Complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import TodoTable from './components/TodoTable';
 function App() {
+  let todos = [
+    {rowNumber:1,rowDescription:'feed puppy',rowAssigned:"aman"},
+    {rowNumber:2,rowDescription:"Get hair cut",rowAssigned:"vivesh"},
+    {rowNumber:3,rowDescription:"water the plant",rowAssigned:"Aditya"},
+    {rowNumber:4,rowDescription:"Come FLy with Me",rowAssigned:"Shobit"},
+    {rowNumber:5,rowDescription:"Get Well soon Mamu",rowAssigned:"Murli Prasad"}
+  ]
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mt-5 container'>
+      <div className ='card'>
+        <div className='card-header'>
+          Your Todo's
+        </div>
+        <div className='card-body'>
+        
+            <TodoTable todos={todos} />
+           
+          
+        </div>
+      </div>
+   
+     
     </div>
   );
 }
---
+++ src/components/TodoTable.js
@@ -0,0 +1,28 @@
+import TodoRowItem from "./TodoRowItem"
+
+function TodoTable(props){
+    return(
+        <table className="table table-hover">
+            <thead>
+                <tr>
+                    <th scope='col'>#</th>
+                    <th scope='col'>Description</th>
+                    <th scope='col'>Assigned User</th>
+                </tr>
+            </thead>
+         <tbody>
+            {props.todos.map(todos=>(
+                <TodoRowItem 
+                rowNumber={todos.rowNumber}
+                rowDescription={todos.rowDescription}
+                rowAssigned = {todos.rowAssigned}
+                />
+            ))}
+                 
+        
+        </tbody>
+            
+        </table>
+    )
+}
+export default TodoTable
